fd85d4b4c15ec146c65c0371cd3f93a149a5e7fe
Valdemar <66011411+testnamepleaseigonre@users.noreply.github.com>
2021-02-22 15:47:17 +0200

    Add files via upload

---
+++ PD1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace PD1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.path = new System.Windows.Forms.TextBox();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(393, 93);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(467, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(291, 55);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(291, 94);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 2;
+            this.openButton.Text = "Choose";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(32, 57);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(253, 20);
+            this.fileNameTextBox.TabIndex = 3;
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(32, 96);
+            this.path.Name = "path";
+            this.path.ReadOnly = true;
+            this.path.Size = new System.Drawing.Size(253, 20);
+            this.path.TabIndex = 4;
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(393, 77);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(52, 13);
+            this.progressLabel.TabIndex = 5;
+            this.progressLabel.Text = "\\directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "File name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Starting path:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(32, 144);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(828, 303);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Found paths:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 459);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Label progressLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PD1/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Security.AccessControl;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PD1
+{
+    public partial class Form1 : Form
+    {
+        private static List<string> finalFileList = new List<string>();
+        private static List<string> finalDirectoryList = new List<string>();
+        private string currentPath;
+        private bool fileFound = false;
+        private bool directoryFound = false;
+        private bool progressBarChange = false;
+        private bool pathChanged = false;
+        private string timeElapsed;
+        private int lenght = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            fileNameTextBox.Text = "cmd";
+            path.Text = "C:\\Windows\\System32";
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if(dialog.ShowDialog(this) == DialogResult.OK)
+                path.Text = dialog.SelectedPath;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {   
+            if (String.IsNullOrWhiteSpace(path.Text) || String.IsNullOrWhiteSpace(fileNameTextBox.Text))
+            {
+                MessageBox.Show("Please fill in required fields!");
+            }
+            else
+            {
+                searchButton.Enabled = false;
+
+                finalFileList.Clear();
+                finalDirectoryList.Clear();
+                currentPath = "";
+                richTextBox1.Text = "";
+
+                Thread th1 = new Thread(() => searchFunc(path.Text.ToString(), fileNameTextBox.Text.ToString()));
+                Thread th2 = new Thread(() => printFunc(th1));
+
+                th1.Start();
+                Console.WriteLine($"Thread {th1.ManagedThreadId} [searching] started");
+                th2.Start();
+                Console.WriteLine($"Thread {th2.ManagedThreadId} [printing] started");
+            }
+        }
+
+        private void searchFunc(string directory, string searchFileName)
+        {
+            var sw = Stopwatch.StartNew();
+
+            lenght = 0;
+            getProgressBarLenght(directory);
+
+            Invoke((Action)delegate
+            {
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = lenght;
+                progressBar1.Value = 0;
+                progressBar1.Step = 1;
+            });
+
+            Console.WriteLine(progressBar1.Maximum.ToString());
+            findPathRecursion(directory, searchFileName);
+
+            //Timer
+            sw.Stop();
+            timeElapsed = $"Time elapsed: {sw.Elapsed.TotalSeconds.ToString("0.##")}s";
+
+            Invoke((Action)delegate
+            {
+                searchButton.Enabled = true;
+            });
+            Console.WriteLine(progressBar1.Value.ToString());
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} [searching] ended");
+        }
+
+        private void findPathRecursion(string directory, string searchFileName)
+        {
+            //Console.WriteLine($"Recursion Func on {Thread.CurrentThread.ManagedThreadId} thread");
+            try
+            {
+                foreach (string dir in Directory.GetDirectories(directory, "*", SearchOption.TopDirectoryOnly))
+                {
+                    findPathRecursion(dir, searchFileName);
+                }
+                progressBarChange = true;
+                currentPath = directory.ToString();
+                pathChanged = true;
+                Thread.Sleep(50);
+                if (directory == Path.GetDirectoryName(directory) + "\\" + searchFileName)
+                {
+                    finalDirectoryList.Add(Path.GetDirectoryName(directory).ToString());
+                    directoryFound = true;
+                    Thread.Sleep(50);
+                }
+                foreach (string file in Directory.GetFiles(directory, "*", SearchOption.TopDirectoryOnly))
+                {
+                    if (searchFileName == Path.GetFileNameWithoutExtension(file))
+                    {
+                        finalFileList.Add(Path.GetDirectoryName(file).ToString());
+                        fileFound = true;
+                        Thread.Sleep(50);
+                    }
+                }
+            }
+            catch(UnauthorizedAccessException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+
+        private void printFunc(Thread th1)
+        {
+            while (th1.IsAlive)
+            {
+                if (fileFound == true)
+                {
+                    try
+                    {
+                        Invoke((Action)delegate
+                        {
+                            richTextBox1.Text += $"File found: {finalFileList[finalFileList.Count - 1]} \n";
+                        });
+                        fileFound = false;
+                    }
+                    catch
+                    {
+                        th1.Abort();
+                    }
+                }
+                if (directoryFound == true)
+                {
+                    try
+                    {
+                        Invoke((Action)delegate
+                        {
+                            richTextBox1.Text += $"Directory found: {finalDirectoryList[finalDirectoryList.Count - 1]} \n";
+                        });
+                        directoryFound = false;
+                    }
+                    catch
+                    {
+                        th1.Abort();
+                    }
+                }
+                if (progressBarChange == true)
+                {
+                    try
+                    {
+                        Invoke((Action)delegate
+                        {
+                            progressBar1.PerformStep();
+                        });
+                        progressBarChange = false;
+                    }
+                    catch
+                    {
+                        th1.Abort();
+                    }
+                }
+                if (pathChanged == true)
+                {
+                   try
+                    {
+                        Invoke((Action)delegate
+                        {
+                            progressLabel.Text = currentPath;
+                        });
+                        pathChanged = false;
+                    }
+                    catch
+                    {
+                        th1.Abort();
+                    }
+                }
+            }
+            try
+            {
+                Invoke((Action)delegate
+                {
+                    if (finalDirectoryList.Count.Equals(0) && finalFileList.Count.Equals(0))
+                    {
+                        richTextBox1.Text = $"No files/directories found! \n";
+                    }
+                    richTextBox1.Text += timeElapsed;
+                });
+            }
+            catch
+            {
+
+            }
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} [printing] ended");
+        }
+
+        private void getProgressBarLenght(string directory)
+        {
+            try
+            {
+                foreach (string dir in Directory.GetDirectories(directory, "*", SearchOption.TopDirectoryOnly))
+                {
+                    
+                    getProgressBarLenght(dir);
+                    
+                }
+                lenght++;
+            }
+            catch(UnauthorizedAccessException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+    }
+}
